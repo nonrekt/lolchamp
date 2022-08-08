30d17d90b58012245db816974c08904f80772757
hordaa <53752987+hordaa@users.noreply.github.com>
2022-08-08 21:29:17 +0200

    Updated setting up lolwatcher

--- API_login.py
+++ API_login.py
@@ -1,9 +1,9 @@
 import json
-
+from riotwatcher import LolWatcher, ApiError
 
 class Login:
     def __init__(self):
-        with open('API_key.json') as f:
+        with open('API_keyjson') as f:
             self.API_KEY = json.load(f)
 
-        self.lolwatcher = LolWatcher(API_KEY)
+        self.lolwatcher = LolWatcher(self.API_KEY)
